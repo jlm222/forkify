42ea29f5aeff268e49008e5bc5b194182ab7a87a
jlm222 <59456904+jlm222@users.noreply.github.com>
2021-04-27 01:18:46 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e4cd6ab7-b875-4555-98e3-b447fe26eb5d';
+export const MODAL_CLOSE_SEC = 1;
